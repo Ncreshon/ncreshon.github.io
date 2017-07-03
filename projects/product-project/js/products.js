356a68ded1929bf34e8abd5a17d750244a295e39
Nicole Creshon <ncreshon@gmail.com>
2017-07-03 20:58:38 +0000

    add tag

---
+++ projects/product-project/js/products.js
@@ -0,0 +1,15 @@
+/* global $ _ */
+
+
+
+$(function () {
+  // ALL YOUR CODE GOES BELOW HERE //
+ $.getJSON('product.json', function(product){
+     
+ }); 
+  
+  
+  
+  
+  // ALL YOUR CODE GOES ABOVE HERE //
+});
